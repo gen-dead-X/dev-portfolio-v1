e21f750862abce091377bcb3d005411a5c7b669e
Joyrudra <joyrudrabws07@gmail.com>
2024-07-02 20:46:40 +0530

    feat : added backgorund blob

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
-import Blob from "./ui/Global/Blob/Blob";
+"use client";
+
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const blob = document.getElementById("blob");
+
+      blob?.animate(
+        { left: `${e.clientX - 150}px`, top: `${e.clientY - 150}px` },
+        { duration: 3000, fill: "forwards" }
+      );
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
-    <div>
-      <Blob />
-    </div>
+    <>
+      <div id="blob" className="blob"></div>
+      <div className="blob-blur__overlay"></div>
+    </>
   );
 }
