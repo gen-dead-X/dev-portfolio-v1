85dc8eeeae2510457122e83eeb3aa48112d7e107
Joyrudra Biswas <joyrudra@itobuz.com>
2024-07-10 18:32:01 +0530

    feat : added cursor for theme toggling animation

--- src/app/ui/Navbar/navbar.tsx
+++ src/app/ui/Navbar/navbar.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { FaLightbulb } from 'react-icons/fa';
 import ThemeToggleButton from '../Theme/ThemeToggleButton';
 
 export default function Navbar() {
   return (
     <div className="relative z-[3] w-full">
-      <div className="flex w-full">
+      <div className="flex w-full justify-end">
         <ThemeToggleButton />
       </div>
     </div>
